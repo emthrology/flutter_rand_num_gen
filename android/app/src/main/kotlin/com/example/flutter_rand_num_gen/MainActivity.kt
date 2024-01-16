d1986cc3e7ccf53d198afdfaa9dcdb8e8ef50925
dongmin kim <emthrology@gmail.com>
2024-01-16 18:42:38 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_rand_num_gen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_rand_num_gen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/homeScreen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('랜덤 숫자 생성'),
+              IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+            ],
+          ),
+          Expanded(
+              child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('123'), Text('456'), Text('789')],
+            ),
+          )),
+          Row(children: [
+            Expanded(
+              child: ElevatedButton(onPressed: () {}, child: Text('CREATE')),
+            )
+          ])
+        ],
+      ),
+    ));
+  }
+}
