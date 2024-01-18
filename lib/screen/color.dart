5fe518b0c13ffeef3efcb3344a44ee82eeb339c3
Dongmin <emthrology@gmail.com>
2024-01-18 16:42:29 +0900

    컬러 프리셋 정하기, 플러터 코드상에서 간단한  map() 함수 사용

---
+++ lib/screen/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color PRIMARY = Color(0xFF2D2D33);
+const Color RED = Color(0xFFEA4955);
+const Color BLUE = Color(0xFF549FBF);
--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rand_num_gen/screen/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,32 +12,65 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: PRIMARY,
         body: SafeArea(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('랜덤 숫자 생성'),
-              IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-            ],
-          ),
-          Expanded(
-              child: SizedBox(
-            width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('123'), Text('456'), Text('789')],
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '랜덤 숫자 생성',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.settings, color: RED))
+                  ],
+                ),
+                Expanded(
+                    child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [123, 675, 789]
+                          .asMap()
+                          .entries
+                          .map((x) => Padding(
+                                padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                                child: Row(
+                                  children: x.value
+                                      .toString()
+                                      .split('')
+                                      .map((x) => Image.asset(
+                                            'asset/img/$x.png',
+                                            height: 70.0,
+                                            width: 50.0,
+                                          ))
+                                      .toList(),
+                                ),
+                              ))
+                          .toList()),
+                )),
+                Row(children: [
+                  Expanded(
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: RED,
+                        ),
+                        onPressed: () {},
+                        child: Text('CREATE')),
+                  )
+                ])
+              ],
             ),
-          )),
-          Row(children: [
-            Expanded(
-              child: ElevatedButton(onPressed: () {}, child: Text('CREATE')),
-            )
-          ])
-        ],
-      ),
-    ));
+          ),
+        ));
   }
 }
